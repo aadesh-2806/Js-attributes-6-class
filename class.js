ff7bfc6257e9916fc6aba4e8418e68fd9152584d
Aadesh Agrawal <74392722+aadesh-2806@users.noreply.github.com>
2021-07-05 22:31:38 +0530

    Add files via upload

---
+++ class.js
@@ -0,0 +1,100 @@
+ const person={
+    firstname: 'dde' ,
+    middlename: 'xcxc',
+    lastname: 'fasd'
+};
+nest=[
+    {
+        przcoduct:'dsd',
+        david:'svc',
+        arjum:'sdsdv'
+    },
+    {
+        producdczxt:'dadxcsd',
+        printzxcg:'sxzcvc',
+        qhduhzxcu:'sdsasddv'
+    },
+    {
+        producvct:'dasdsd',
+        printssg:'ssasavc',
+        qhduhZXu:'szxcdsdv'
+    }
+]
+
+/*
+nest[1]
+{producdczxt: "dadxcsd", printzxcg: "sxzcvc", qhduhzxcu: "sdsasddv"}
+nest[1].printzxcg
+"sxzcvc"
+*/
+//////////////////////////////////////////////class/////////////////////////////////////////////////////////////
+
+/*
+const person={
+    firstname: 'dde' ,
+    middlename: 'xcxc',
+    lastname: 'fasd'
+};
+
+person.firstname
+"dde"
+
+person["firstname"]
+"dde"
+
+person['first' + 'name']
+"dde"
+
+person[firstname]
+VM136:1 Uncaught ReferenceError: firstname is not defined
+    at <anonymous>:1:8
+(anonymous) @ VM136:1
+
+t={
+    1990:"dfhuh",
+    1991:"Next Year to !(()"
+};
+{1990: "dfhuh", 1991: "Next Year to !(()"}
+
+t[1990]
+"dfhuh"
+
+t["1990"]
+"dfhuh"
+
+t.1990
+VM327:1 Uncaught SyntaxError: Unexpected number
+*/
+///////////////////////////////////////////////////class with const/////////////////////////////////////
+
+/*
+const mid={dan: 96,thomas:98};
+undefined
+
+mid.thomas
+98
+
+mid.thomas=99
+99
+
+mid["dany"]=87
+87
+
+mid
+{dan: 96, thomas: 99, dany: 87}
+
+mid.ezra=89                                 //pushing no's
+89
+
+mid.hgcvb=87.5
+87.5
+
+mid
+{dan: 96, thomas: 99, dany: 87, ezra: 89, hgcvb: 87.5}
+
+t={
+    1990:"dfhuh",
+    1991:"Next Year to !(()",
+};
+{1990: "dfhuh", 1991: "Next Year to !(()"}
+*/
